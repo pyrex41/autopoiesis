79bf6da433fb6610183ad483b0f655d3a4fc619a
pyrex41 <reub.brooks@gmail.com>
2026-02-17 11:43:53 -0600

    Revert "Add Bevy-based 3D frontend (Holodeck) for Autopoiesis platform"

--- holodeck/src/systems/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod agents;
-pub mod animation;
-pub mod blocking;
-pub mod connection;
-pub mod layout;
-pub mod selection;
-pub mod snapshots;
-pub mod thoughts;
--- holodeck/src/ui/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod agent_panel;
-pub mod command_bar;
-pub mod hud;
-pub mod notifications;
-pub mod thought_inspector;
